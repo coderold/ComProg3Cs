7996b2095d677311be9dd2b9e4a2ef227459fe7b
coderold <92320371+coderold@users.noreply.github.com>
2024-10-13 23:55:05 +0800

    bukas naman

--- ComProg3C#/Act07-01/Act07-01.cs
+++ ComProg3C#/Act07-01/Act07-01.cs
@@ -1,14 +1,62 @@
 ﻿using System;
+using System.Data;
 
 namespace UserNamespace
 {
     public class User
     {
+        private string user_id;
+        protected string user_password;
+
+        public User(string id, string pass)
+        {
+            this.user_id = id;
+            this.user_password = pass;
+        }
+
+        public bool verifyLogin(string id, string pass)
+        {
+            if (this.user_id.Equals(id) && this.user_password.Equals(pass))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void updatePassword(string newPassword)
+        {
+            this.user_password = newPassword;
+        }
 
     }
 
-    public class Administrator
+    public class Administrator : User
     {
+        private string admin_name { get; set; }
 
+        public Administrator(string name, string id, string pass) : base(id, pass)
+        {
+            this.admin_name = name;
+        }
+
+        public void updateAdminName(string name)
+        {
+            this.admin_name = name;
+        }
     }
+
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            User usr = new User("admin", "123");
+
+            
+
+        }
+    }
+
 }
