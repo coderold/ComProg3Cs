08bdd34e397c7774216781814b6fc38fcad6ed34
coderold <92320371+coderold@users.noreply.github.com>
2024-10-14 14:39:15 +0800

    LabEx07-01 setup

---
+++ ComProg3C#/LabEx07-01/LabEx07-01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ItemNamespace
+{
+    public abstract class Item
+    {
+
+    }
+
+    public class DiscountedItem : Item
+    {
+
+    }
+
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello setup");
+        }
+    }
+}
